78dad5ebaccae114e94713b8431eb87aa6534250
Josh <chaozhang78@icloud.com>
2018-01-13 17:49:14 -0500

    Configured vscode running env.

--- src/test/java/P21_P40/P21_Merge_Two_Sorted_Lists/SolutionTest.java
+++ src/test/java/P21_P40/P21_Merge_Two_Sorted_Lists/SolutionTest.java
@@ -2,8 +2,6 @@ package P21_P40.P21_Merge_Two_Sorted_Lists;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SolutionTest {
 
     @Test
--- src/test/java/P21_P40/P27_Remove_Element/SolutionTest.java
+++ src/test/java/P21_P40/P27_Remove_Element/SolutionTest.java
@@ -2,8 +2,6 @@ package P21_P40.P27_Remove_Element;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class SolutionTest {
 
     @Test
