8754a887cdc75a848de220c77739813823f41ac6
Josh <chaozhang78@icloud.com>
2018-02-12 21:05:23 -0500

    Added comments to P19

--- src/main/java/P1_P20/P19_Remove_Nth_Node_From_End_of_List/Solution.java
+++ src/main/java/P1_P20/P19_Remove_Nth_Node_From_End_of_List/Solution.java
@@ -6,21 +6,29 @@ import common.ListNode;
  * Created by czhang on 12/29/2016.
  */
 public class Solution {
+    /**
+     * The key to find the node before the nth node from end
+     * @param head
+     * @param n
+     * @return
+     */
     public ListNode removeNthFromEnd(ListNode head, int n) {
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode first = dummy;
         ListNode second = dummy;
-        // Advances first pointer so that the gap between first and second is n nodes apart
-        for (int i = 1; i <= n + 1; i++) {
+        // Advances first pointer n nodes
+        for (int i = 0; i < n; i++) {
             first = first.next;
         }
-        // Move first to the end, maintaining the gap
+        // Move first to the end, maintaining the gap n
         while (first != null) {
             first = first.next;
             second = second.next;
         }
+        // Remove the node
         second.next = second.next.next;
+        // Return the head
         return dummy.next;
     }
 }
